7a7857a7d3b0c9bfd0a6ee7a55a31db014f6ab7d
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-02-16 21:20:39 -0800

    starting on sidebar content

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -2,7 +2,19 @@ import React from "react";
 import styles from "../../styles/Sidebar.module.css";
 
 const SideBar = () => {
-  return <nav className={styles.Sidebar__container}>test</nav>;
+  return (
+    <nav className={styles.Sidebar__container}>
+      <div className="fr">
+        <div className={styles.company__logo}></div>
+        <h1>Company Name</h1>
+      </div>
+      <div className={styles.search__bar}></div>
+      <div className="fr"></div>
+      <div className="fr"></div>
+      <div className="fr"></div>
+      <div className="fr"></div>
+    </nav>
+  );
 };
 
 export default SideBar;
