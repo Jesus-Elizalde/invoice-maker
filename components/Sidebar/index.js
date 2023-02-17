8f81aa72757589ebc668035f287927d4e99c8ae9
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-02-16 23:25:07 -0800

    added space between the main tabs and settings tab

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -11,38 +11,42 @@ import SignOutIcon from "../../assets/svgs/SignOutIcon.svg";
 const SideBar = () => {
   return (
     <nav className={styles.Sidebar__container}>
-      <div className={styles.logo__conatiner}>
-        <div className={styles.company__logo}></div>
-        <h1 className={styles.logo__name}>Company Name</h1>
-      </div>
-      <div className={styles.search__bar}></div>
-      <div className={styles.tab__conatiners}>
-        <HomeIcon />
-        <h2>Home</h2>
-      </div>
-      <div className={styles.tab__conatiners}>
-        <CustomersIcon />
-        <h2>Customers</h2>
-      </div>
-      <div className={styles.tab__conatiners}>
-        <EstimatesIcon />
-        <h2>Estimates</h2>
-      </div>
-      <div className={styles.tab__conatiners}>
-        <InvoicesIcon />
-        <h2>Invoices</h2>
-      </div>
-      <div className={styles.tab__conatiners}>
-        <SettingsIcon />
-        <h2>Settings</h2>
-      </div>
-      <div className={styles.profile__conatiner}>
-        <div className={styles.profile__picture}></div>
-        <div className="fc">
-          <p className={styles.username}>Test User</p>
-          <p className={styles.email}>demouser@test.com</p>
+      <div>
+        <div className={styles.logo__conatiner}>
+          <div className={styles.company__logo}></div>
+          <h1 className={styles.logo__name}>Company Name</h1>
         </div>
-        <SignOutIcon />
+        <div className={styles.search__bar}></div>
+        <div className={styles.tab__conatiners}>
+          <HomeIcon />
+          <h2>Home</h2>
+        </div>
+        <div className={styles.tab__conatiners}>
+          <CustomersIcon />
+          <h2>Customers</h2>
+        </div>
+        <div className={styles.tab__conatiners}>
+          <EstimatesIcon />
+          <h2>Estimates</h2>
+        </div>
+        <div className={styles.tab__conatiners}>
+          <InvoicesIcon />
+          <h2>Invoices</h2>
+        </div>
+      </div>
+      <div>
+        <div className={styles.tab__conatiners}>
+          <SettingsIcon />
+          <h2>Settings</h2>
+        </div>
+        <div className={styles.profile__conatiner}>
+          <div className={styles.profile__picture}></div>
+          <div className="fc">
+            <p className={styles.username}>Test User</p>
+            <p className={styles.email}>demouser@test.com</p>
+          </div>
+          <SignOutIcon />
+        </div>
       </div>
     </nav>
   );
