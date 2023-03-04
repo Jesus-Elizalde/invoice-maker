2c4955e92377c579479f50e6434042a0ee2b058d
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-03-03 22:18:46 -0800

    fixed the main tag css when not auth

--- components/layout.jsx
+++ components/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SideBar from "./Sidebar";
 import styles from "../styles/Sidebar.module.css";
-
+import { useRouter } from "next/router";
 import Head from "next/head";
 import { useSession } from "next-auth/react";
 
@@ -16,7 +16,13 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {status === "authenticated" && <SideBar />}
-      <main className={styles.main}>{children}</main>
+      <main
+        className={
+          status === "authenticated" ? styles.main : styles.main__no__nav
+        }
+      >
+        {children}
+      </main>
     </div>
   );
 }
